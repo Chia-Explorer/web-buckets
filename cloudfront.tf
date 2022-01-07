8ec96337ea14447896dc6c941a65a7bb070d931a
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-01-07 14:48:01 +0000

    https-only

--- cloudfront.tf
+++ cloudfront.tf
@@ -25,7 +25,7 @@ resource "aws_cloudfront_distribution" "this" {
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "https-only"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
